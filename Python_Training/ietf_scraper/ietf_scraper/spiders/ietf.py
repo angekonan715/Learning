6a656ff39d36011d888e95e092e11058a1204171
Kouadio Ange Konan <kou20004@byui.edu>
2025-04-17 15:42:17 -0600

    still working on scrapping pythonscraping.com/linkedin/ietf.html

--- Python_Training/ietf_scraper/ietf_scraper/spiders/ietf.py
+++ Python_Training/ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -9,5 +9,9 @@ class IetfSpider(scrapy.Spider):
     def parse(self, response):
         subheading = response.xpath('//span[@class = "subheading"]/text()').getall()
         author_name = response.xpath('//span[@class = "author-name"]/text()').get()
+        date = response.xpath('//span[@class= "date"]/text()').get()
+        author_company = response.xpath('//span[@class = "author-company"]/text()').get()
         return {"subhead": subheading, 
-                "Author_name": author_name}
+                "Author_name": author_name, 
+                "date": date,
+                "author_company": author_company}
